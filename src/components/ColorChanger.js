67dbc28a0b6d2865e239823f3efbfc17cbafb786
sam-obembe <seyanuobembe@gmail.com>
2019-01-11 13:33:54 -0600

    update components with functionality

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function ColorChanger() {
+export default function ColorChanger(props) {
   return (
-    <select className="dropDownContainer">
+    <select className="dropDownContainer" onChange = {(ev) =>props.update(ev.target.value) }>
       <option value="black"> Black </option>
       <option value="blue"> Blue </option>
       <option value="green"> Green </option>
